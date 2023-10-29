2daee75a0022cdcd1eab5520dbf36213979a08b8
Victor Saraev <vvsar@mail.ru>
2023-10-29 22:41:36 +0300

    feat: add ppettier

--- rsschool-react-vvsar/.eslintrc.cjs
+++ rsschool-react-vvsar/.eslintrc.cjs
@@ -1,26 +1,39 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true, jest: true, node: true },
-  extends: [
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "prettier",
+        "plugin:prettier/recommended",
     ],
-    '@typescript-eslint/no-explicit-any': 'error',
-  },
-  settings: {
-    'react': {
-      'pragma': 'React',
-      'version': 'detect'
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "prettier"
+    ],
+    "rules": {
+      "@typescript-eslint/no-explicit-any": "error",
     }
-  },
 }
