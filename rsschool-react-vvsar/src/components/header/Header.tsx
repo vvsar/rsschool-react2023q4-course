9a1aa7deeb6206ca18e1bfdab62f155bbc3628e7
Victor Saraev <vvsar@mail.ru>
2023-10-31 16:32:08 +0300

    feat: change functional componr=ent for class one

--- rsschool-react-vvsar/src/components/header/Header.tsx
+++ rsschool-react-vvsar/src/components/header/Header.tsx
@@ -1,14 +1,19 @@
+import React from "react";
 import "./Header.css";
 
-export default function Header() {
-  return (
-    <header className="header">
-      <div className="search-line">
-        <form className="search-form">
-          <input className="search-input" type="search"></input>
-          <button className="search-button" type="submit"></button>
-        </form>
-      </div>
-    </header>
-  );
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="search-line">
+          <form className="search-form">
+            <input className="search-input" type="search"></input>
+            <button className="search-button" type="submit"></button>
+          </form>
+        </div>
+      </header>
+    );
+  }
 }
+
+export default Header;
