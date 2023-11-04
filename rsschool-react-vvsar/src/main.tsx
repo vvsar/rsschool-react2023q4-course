98f54cbc149d5b305727b7c9a104d56809899ea5
Victor Saraev <vvsar@mail.ru>
2023-11-04 03:11:20 +0300

    feat: add error boundary

--- rsschool-react-vvsar/src/main.tsx
+++ rsschool-react-vvsar/src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ErrorBoundary from "./components/error-boundary/ErrorBoundary.tsx";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
