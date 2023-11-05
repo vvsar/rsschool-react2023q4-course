9265b9cdd709df4de703586c43fbde52d79e242d
Victor Saraev <vvsar@mail.ru>
2023-11-06 01:36:17 +0300

    feat: re-write Header as functional component

--- rsschool-react-vvsar/src/components/header/Header.tsx
+++ rsschool-react-vvsar/src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import "./Header.css";
 
 type HeaderProps = {
@@ -6,46 +6,75 @@ type HeaderProps = {
   handleSubmit: (value: string) => void;
 };
 
-type SearchBarState = {
-  keyWord: string;
-};
+// type SearchBarState = {
+//   keyWord: string;
+// };
 
 // type State = Readonly<SearchBarState>;
 
-class Header extends React.Component<HeaderProps, SearchBarState> {
-  constructor(props: HeaderProps) {
-    super(props);
-    this.state = { keyWord: this.props.keyWord };
-    this.updateKeyWord = this.updateKeyWord.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+// class Header extends React.Component<HeaderProps, SearchBarState> {
+//   constructor(props: HeaderProps) {
+//     super(props);
+//     this.state = { keyWord: this.props.keyWord };
+//     this.updateKeyWord = this.updateKeyWord.bind(this);
+//     this.onSubmit = this.onSubmit.bind(this);
+//   }
 
-  updateKeyWord(e: FormEvent<HTMLInputElement>) {
-    this.setState({ keyWord: (e.target as HTMLInputElement).value });
-  }
+//   updateKeyWord(e: FormEvent<HTMLInputElement>) {
+//     this.setState({ keyWord: (e.target as HTMLInputElement).value });
+//   }
 
-  onSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    this.props.handleSubmit(this.state.keyWord.trim());
+//   onSubmit = (event: React.FormEvent) => {
+//     event.preventDefault();
+//     this.props.handleSubmit(this.state.keyWord.trim());
+//   };
+
+//   render() {
+//     return (
+//       <header className="header">
+//         <div className="search-line">
+//           <form className="search-form" onSubmit={this.onSubmit}>
+//             <input
+//               className="search-input"
+//               type="search"
+//               value={this.state.keyWord}
+//               onChange={this.updateKeyWord}
+//             ></input>
+//             <button className="search-button" type="submit"></button>
+//           </form>
+//         </div>
+//       </header>
+//     );
+//   }
+// }
+
+export default function Header({ keyWord, handleSubmit }: HeaderProps) {
+  const [searchInputValue, setSearchInputValue] = useState(keyWord);
+
+  const updateKeyWord = (e: FormEvent<HTMLInputElement>) => {
+    setSearchInputValue((e.target as HTMLInputElement).value);
   };
 
-  render() {
-    return (
-      <header className="header">
-        <div className="search-line">
-          <form className="search-form" onSubmit={this.onSubmit}>
-            <input
-              className="search-input"
-              type="search"
-              value={this.state.keyWord}
-              onChange={this.updateKeyWord}
-            ></input>
-            <button className="search-button" type="submit"></button>
-          </form>
-        </div>
-      </header>
-    );
-  }
+  const onSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    handleSubmit(searchInputValue.trim());
+  };
+
+  return (
+    <header className="header">
+      <div className="search-line">
+        <form className="search-form" onSubmit={onSubmit}>
+          <input
+            className="search-input"
+            type="search"
+            value={searchInputValue}
+            onChange={updateKeyWord}
+          ></input>
+          <button className="search-button" type="submit"></button>
+        </form>
+      </div>
+    </header>
+  );
 }
 
-export default Header;
+// export default Header;
