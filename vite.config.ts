a0c4a13e1f1be36f6a0fb39b88ddf80ec812530a
Victor Saraev <vvsar@mail.ru>
2023-11-06 14:42:12 +0300

    fix: change test script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/rsschool-react2023q4-course",
+  base: "/",
   plugins: [react()],
 });
