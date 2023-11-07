c49c6fcdfba75fab56606a4c4bfd2893f917eead
Victor Saraev <vvsar@mail.ru>
2023-11-07 16:40:54 +0300

    feat: add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/header/Header";
 import Results from "./components/results/Results";
 import "./App.css";
-
-// type AppProps = Record<string, never>;
-
-// type AppState = {
-//   searchInputValue: string;
-// };
-
-// class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-//     this.state = { searchInputValue: localStorage.getItem("keyWord") || "" };
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleSubmit(value: string) {
-//     localStorage.setItem("keyWord", value);
-//     this.setState({ searchInputValue: value });
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         <Header
-//           keyWord={this.state.searchInputValue}
-//           handleSubmit={this.handleSubmit}
-//         />
-//         <Results word={this.state.searchInputValue} />
-//       </>
-//     );
-//   }
-// }
 
 export default function App() {
   const initialSearchValue = localStorage.getItem("keyWord") || "";
@@ -52,18 +22,36 @@ export default function App() {
 
   return (
     <>
-      <p style={{ color: "red" }}>
-        <b>Module02 is not finished. Please check later!</b>
-      </p>
-      <Header
-        keyWord={searchInputValue}
-        perPage={perPageValue}
-        handleSubmit={handleSubmit}
-        handlePerPageChoice={handlePerPageChoice}
-      />
-      <Results word={searchInputValue} perPage={perPageValue} />
+      <Routes>
+        <Route
+          path="rsschool-react2023q4-course/"
+          element={
+            <>
+              <p style={{ color: "red" }}>
+                <b>Module02 is not finished. Please check later!</b>
+              </p>
+              <Header
+                keyWord={searchInputValue}
+                perPage={perPageValue}
+                handleSubmit={handleSubmit}
+                handlePerPageChoice={handlePerPageChoice}
+              />
+              <Results word={searchInputValue} perPage={perPageValue} />
+            </>
+          }
+        />
+      </Routes>
     </>
   );
 }
 
-// export default App;
+// return (
+//   <>
+//     <Routes>
+//       <Route
+//         path="rsschool-react2023q4-course"
+//         element={<Results word={searchInputValue} perPage={perPageValue} />}
+//       />
+//     </Routes>
+//   </>
+// );
