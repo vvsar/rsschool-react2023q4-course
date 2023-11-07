602b481b9bcd4dc7d140804f3d88fcdfbca24180
Victor Saraev <vvsar@mail.ru>
2023-11-07 03:53:58 +0300

    fix: change vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "rsschool-react2023q4-course",
   plugins: [react()],
 });
