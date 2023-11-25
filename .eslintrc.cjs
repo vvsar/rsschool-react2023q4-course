36a3f7619ff1ac014748659943f66e5c74022552
Victor Saraev <vvsar@mail.ru>
2023-11-25 03:46:20 +0300

    feat: install project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,8 @@ module.exports = {
     node: true,
   },
   extends: [
+    "next",
+    "next/core-web-vitals",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/rsschool-react2023q4-course/",
-  plugins: [react()],
-});
