ea4d8d9efe3066bcd3b6ab7ef9880447cb6bf5c3
Victor Saraev <vvsar@mail.ru>
2023-12-04 09:14:02 +0300

    feat: implement yup to form2

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,10 +1,13 @@
 export type UserData = {
   name: string;
-  age: number | string;
+  age: number;
   email: string;
   gender: string;
   country: string;
   // photo: string;
+  password: string;
+  confirm_password: string;
+  accept?: boolean;
 };
 
 export type ResultsState = {
